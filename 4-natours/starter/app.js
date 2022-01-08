16779b77b0d69ecbf84c2d015a7308744fce16bf
khoshzahmat <amin.k@bugloos.com>
2022-01-08 21:01:20 +0330

    How middleware works

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,41 +1,64 @@
+/**
+ * - If no route found, express will handle it with default not route found.
+ * - Add headers to response like .json --> Content-Type: application/json, X-Powered-By: Express
+ * - /:id/:x/:y?  > with ? we can make a variable optional in URL
+ *
+ */
+
+/**
+ * Require modules
+ */
 const express = require('express');
 const fs = require('fs');
 
+/**
+ * Setup Server and Middleware,
+ * Everything is middleware, event routers
+ * All the middlewares together that we use in our app, is called MIDDLEWARE STACK
+ */
 const app = express();
-/**
- * This is a middleware stands between request and response.
- */
-app.use(express.json());
 const port = 3000;
+app.use(express.json()); //  To get access to the request body on the request object, This is a middleware stands between request and response.
 
 /**
- * If no route found, express will handle it with default not route found.
- * Add headers to response like .json --> Content-Type: application/json, X-Powered-By: Express
+ * - Custom Middleware
+ * - Never forget to call next(), because without that response will stop at this moment, it won't pass it to next middleware in Middleware stack.
+ * - The priority of middlewares are important, they can run step by step after each other. (e.g. put it after routes.)
+ *   because middleware cycle will finish.
  */
+app.use((req, res, next) => {
+  console.log('hello from middleware');
+  next();
+});
 
-// app.get('/', (req, res) => {
-//   //   res.status(200).send('Hello from the server side');
-//   res
-//     .status(200)
-//     .json({ message: 'Hello from the server side', app: 'Natours' });
-// });
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  req.zart = new Date().toISOString();
+  next();
+});
 
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint...');
-// });
-
+/**
+ * DATA
+ */
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
-app.get('/api/v1/tours', (req, res) => {
+
+/**
+ * Route functions
+ */
+const getAllTours = (req, res) => {
+  console.log(req.requestTime);
+  console.log(req.zart);
   res.status(200).json({
     status: 'success',
     results: tours.length, // It's not in JSend specification, but helps when we are dealing with arrays
+    requestedAt: req.zart,
     data: {
       tours,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const createTour = (req, res) => {
   console.log(req.body);
   const newID = tours[tours.length - 1].id - 1;
   const newTour = Object.assign({ id: newID }, req.body);
@@ -54,12 +77,9 @@ app.post('/api/v1/tours', (req, res) => {
       },
     });
   });
-});
+};
 
-/**
- * /:id/:x/:y?  > with ? we can make a variable optional in URL
- */
-app.get('/api/v1/tours/:id', (req, res) => {
+const getTour = (req, res) => {
   console.log(req.params);
   const id = req.params.id * 1; // convert it to number with * 1
   const tour = tours.find((el) => el.id === id);
@@ -76,18 +96,20 @@ app.get('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: { tour },
   });
-});
+};
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+const updateTour = (req, res) => {
   const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id === id);
-
-  if (!tour) {
+  if (!tours) {
     return res.status(404).json({
       status: 'fail',
-      message: 'Invalid ID',
+      message: 'Invalid',
     });
   }
+
+  const tour = tours.find((el) => el.id === id);
+  const duration = req.body.duration;
+  const updatedTour = { ...tour, duration };
 
   res.status(200).json({
     status: 'success',
@@ -95,7 +117,43 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: 'tour data',
     },
   });
-});
+};
+
+const deleteTour = (req, res) => {
+  const id = req.params.id * 1;
+  if (!tours) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid',
+    });
+  }
+
+  // const tour = tours.find((el) => el.id === id);
+  // const duration = req.body.duration;
+  // const updatedTour = { ...tour, duration };
+
+  /**
+   * 204 returns nothing
+   */
+  res.status(204).json({
+    status: 'success',
+    data: {
+      tour: { name: 'amin' },
+    },
+  });
+};
+
+/**
+ * API Routes
+ */
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', createTour);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', updateTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app.route('/api/v1/tours/:id').get(getTour).patch(updateTour).delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
