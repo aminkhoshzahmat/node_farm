f4aa4e178238a0696f43a30d9eb3cfeaf4144c54
khoshzahmat <amin.k@bugloos.com>
2022-01-11 07:27:27 +0330

    Introduction to MongoDB

--- 4-natours/starter/Note.js
+++ 4-natours/starter/Note.js
@@ -9,4 +9,11 @@
  * https://eslint.org/docs/rules/
  * files with error will get red.
  * npm i eslint prettier eslint-config-prettier eslint-plugin-prettier  eslint-config-airbnb eslint-plugin-node eslint-plugin-import eslint-plugin-jsx-a11y eslint-plugin-react --save-dev
+ *
+ * ========================================
+ * MongoDB
+ *  Maximum size for each document is currently 16MB
+ * Each documents contains unique id which acts as primary key of that document.
+ *
+ *
  */
