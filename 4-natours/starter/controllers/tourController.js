2cb655a0aa83811637315f0076e49331fb42b4ec
khoshzahmat <amin.k@bugloos.com>
2022-01-19 06:54:02 +0330

    Read all with find(), and Read one with findById()

--- 4-natours/starter/controllers/tourController.js
+++ 4-natours/starter/controllers/tourController.js
@@ -22,17 +22,23 @@ exports.checkID = (req, res, next, val) => {
 /**
  * Route Handlers
  */
-exports.getAllTours = (req, res) => {
-  console.log(req.requestTime);
-  console.log(req.zart);
-  res.status(200).json({
-    status: 'success',
-    results: tours.length, // It's not in JSend specification, but helps when we are dealing with arrays
-    requestedAt: req.zart,
-    data: {
-      tours,
-    },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      status: 'success',
+      results: tours.length, // It's not in JSend specification, but helps when we are dealing with arrays
+      requestedAt: req.zart,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 /**
@@ -60,21 +66,32 @@ exports.createTour = async (req, res) => {
     });
   } catch (err) {
     res.status(400).json({
-      status: 'failed',
+      status: 'fail',
       message: 'Invalid data sent',
     });
   }
 };
 
-exports.getTour = (req, res) => {
-  console.log(req.params);
-  const id = req.params.id * 1; // convert it to number with * 1
-  const tour = tours.find((el) => el.id === id);
-
-  res.status(200).json({
-    status: 'success',
-    data: { tour },
-  });
+/**
+ * req.body > what we sent through postman
+ * req.params > what we get from route like :id
+ */
+exports.getTour = async (req, res) => {
+  try {
+    // Tour.findOne({_id: req.params.id})
+    const tour = await Tour.findById(req.params.id); // a shorter way of findOne
+    res.status(200).json({
+      status: 'success',
+      date: {
+        tour: tour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
 
 exports.updateTour = (req, res) => {
