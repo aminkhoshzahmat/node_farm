9e0e65152295e3c893bd6f0a5b51790b8ccae37d
Amin Khoshzahmat <khoshzahmat@bugloos.nl>
2022-06-20 00:28:11 +0430

    error handlers
    invalid DB IDs, duplicate DB fields, mongoose validation errors, unhandledRejection, uncaughtException

--- 4-natours/starter/controllers/errorController.js
+++ 4-natours/starter/controllers/errorController.js
@@ -1,3 +1,23 @@
+const AppError = require('../utils/appError');
+
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}.`;
+  return new AppError(message, 400);
+};
+
+const handleDuplicateFieldDB = (err) => {
+  const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
+  const message = `Duplicate field value: ${value}. Please use another values`;
+  return new AppError(message, 400);
+};
+
+const handleValidationErrorDB = (err) => {
+  console.log(err.erros);
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -30,6 +50,7 @@ const sendErrorProd = (err, res) => {
  * NOTE: Middleware with 4 arguments, express automatically recognise it as
  * error handling middleware. Therefor it will call it, when there is an error.
  * error first argument.
+ * By default, we assume that errors are operational.
  */
 module.exports = (err, req, res, next) => {
   // console.log(err.stack); // shows where the error occurred.
@@ -39,6 +60,17 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    // https://www.udemy.com/course/nodejs-express-mongodb-bootcamp/learn/lecture/15065206#questions/11418172
+    // In new versions, "name" and "message" properties don't exist anymore after "deconstructing" the "err" argument.
+    // let error = { ...err }; // we don't want to override actual express err.
+
+    let error = { name: err.name, message: err.message, errors: err.errors };
+    error = Object.assign(error, err);
+
+    if (error.name === 'CastError') error = handleCastErrorDB(error); // operational error handler for mongoose
+    if (error.code === 11000) error = handleDuplicateFieldDB(error); // operational error handler for mongo
+    if (error.name === 'ValidationError') error = handleValidationErrorDB(error); // operational error handler for mongoose
+    console.log(error.errors);
+    sendErrorProd(error, res);
   }
 };
--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -6,6 +6,20 @@ const dotenv = require('dotenv');
  */
 dotenv.config({ path: './config.env' });
 
+/**
+ * Catching uncaught exception
+ * use case: refrerence to something that is not declared, console.log(x)
+ * NOTE: this listener should be at the top of the app, this way they can catch everything.
+ * NOTE: we don't need to shutdown gracefully! because app itself cannot be run with this situation.
+ * NOTE: if console.log(x) is inside of any other middleware function, then errorController can't handle it with
+ *       our defined handled... methods, it just simply pass the error to console depend on dev or prod mode.
+ */
+process.on('uncaughtException', (err) => {
+  console.log(`UNCAUGHT EXCEPTION! 🔥 Shutting down...`);
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 const app = require('./app');
 
 /**
@@ -25,6 +39,7 @@ mongoose
     useNewUrlParser: true,
   })
   .then(() => console.log(`DB connected successfully to: ${process.env.DATABASE_NAME}`));
+// .catch(() => console.log('error')); // but we want to handle it globaly, unhandled rejection
 
 /**
  * Default environment is 'development'
@@ -38,6 +53,21 @@ mongoose
  * Start the Server
  */
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}`);
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port} (${process.env.NODE_ENV})`);
+});
+
+/**
+ * Use Case: wrong password for mongodb connection in config.env, we can catch in connection setting,
+ * but we are looking a global solution for unhandled rejection.
+ * NOTE: there is nothing we can do here, it's better to shutdown the application but gracefully!
+ *    server.close => this try to close app gracefully, finish all request that are pending then execute the callback function
+ *    process.exit() => shutdown app immediatelly
+ */
+process.on('unhandledRejection', (err) => {
+  console.log(`UNHANDLED REJECTION! 🔥 Shutting down...`);
+  console.log(err.name, err.message);
+  server.close(() => {
+    process.exit(1);
+  });
 });
